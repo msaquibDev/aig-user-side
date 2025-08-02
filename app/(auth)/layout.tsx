df54fd017683a3f0708fcb9cc12a4959e82ceceb
Mohammad Saquib <md.saquib@saascraft.studio>
2025-08-02 12:01:15 +0530

    feat: fix error

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import { SessionProvider } from "next-auth/react";
-=======
-<<<<<<< HEAD
->>>>>>> 30caa6215b3a63fa0aababb2fb18818abec895a1
 import { Toaster } from "react-hot-toast";
 
 export default function AuthLayout({
@@ -14,21 +9,7 @@ export default function AuthLayout({
     <html lang="en">
       <body>
         <Toaster position="top-right" />
-<<<<<<< HEAD
-        <SessionProvider>{children}</SessionProvider>
-=======
         {children}
-=======
-// app/layout.tsx
-import { SessionProvider } from 'next-auth/react';
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <SessionProvider>{children}</SessionProvider>
->>>>>>> backend
->>>>>>> 30caa6215b3a63fa0aababb2fb18818abec895a1
       </body>
     </html>
   );
