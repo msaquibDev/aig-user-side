4e3553e55819eb6ef8d5b0e66e35a67a0fcf3552
Mohammad Saquib <md.saquib@saascraft.studio>
2025-08-04 16:01:47 +0530

    fix runtime error

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "../../globals.css";
+import { Suspense } from "react";
+
 import ResetPassword from "@/components/auth/ResetPassword";
 
 export default function ResetPasswordPage() {
-  return <ResetPassword />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPassword />
+    </Suspense>
+  );
 }
