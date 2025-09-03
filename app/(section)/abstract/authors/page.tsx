35d78794d7a0242a6f67db495a5c7fe5ed4605c7
Mohammad Saquib <md.saquib@saascraft.studio>
2025-09-03 12:25:27 +0530

    fix deployment error

--- app/(section)/abstract/authors/page.tsx
+++ app/(section)/abstract/authors/page.tsx
@@ -1,40 +1,39 @@
 "use client";
 
-
 import { useState } from "react";
 import { useAuthorStore } from "@/app/store/useAuthorStore";
 import MyAuthorsTable from "@/components/abstract/authors/MyAuthorsTable";
 import AuthorFormSidebar from "@/components/abstract/authors/AuthorFormSidebar";
 
 export default function MyAuthorsPage() {
-  // const [open, setOpen] = useState(false);
-  // const [editId, setEditId] = useState<number | null>(null);
-  // const { getAuthorById } = useAuthorStore();
-  // const defaultData = editId ? getAuthorById(editId) : null;
+  const [open, setOpen] = useState(false);
+  const [editId, setEditId] = useState<number | null>(null);
+  const { getAuthorById } = useAuthorStore();
+  const defaultData = editId ? getAuthorById(editId) : null;
 
-  // const handleEditClick = (id: number) => {
-  //   setEditId(id);
-  //   setOpen(true);
-  // };
+  const handleEditClick = (id: number) => {
+    setEditId(id);
+    setOpen(true);
+  };
 
-  // const handleViewClick = (id: number) => {
-  //   // Logic for viewing the abstract details (could be a modal or page redirection)
-  //   console.log("Viewing abstract:", id);
-  // };
+  const handleViewClick = (id: number) => {
+    // Logic for viewing the abstract details (could be a modal or page redirection)
+    console.log("Viewing abstract:", id);
+  };
 
-  // return (
-  //   <>
-  //     <MyAuthorsTable onEdit={handleEditClick} onView={handleViewClick} />
+  return (
+    <>
+      <MyAuthorsTable onEdit={handleEditClick} onView={handleViewClick} />
 
-  //     {/* Abstract Form Sidebar (Add/Edit) */}
-  //     <AuthorFormSidebar
-  //       open={open}
-  //       onClose={() => {
-  //         setOpen(false);
-  //         setEditId(null);
-  //       }}
-  //       defaultData={defaultData}
-  //     />
-  //   </>
-  // );
+      {/* Abstract Form Sidebar (Add/Edit) */}
+      <AuthorFormSidebar
+        open={open}
+        onClose={() => {
+          setOpen(false);
+          setEditId(null);
+        }}
+        defaultData={defaultData}
+      />
+    </>
+  );
 }
